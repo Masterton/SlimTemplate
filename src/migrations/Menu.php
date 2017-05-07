79f6af5dd31c9b8e190cd853ecc3391d31ed7582
Masterton <987859263@qq.com>
2017-05-07 21:57:53 +0800

    modify model menu

--- src/migrations/Menu.php
+++ src/migrations/Menu.php
@@ -12,7 +12,6 @@ class Menu extends Base
         $this->schema->create($this->table_name, function(\Illuminate\Database\Schema\Blueprint $table) {
             $table->increments('id')
                 ->comment('主键ID');
-            # http://stackoverflow.com/questions/37662955/laravel-migration-default-value
             $table->string('uid', 11)
                 ->comment('用户id');
             $table->string('name', 128)
@@ -20,10 +19,10 @@ class Menu extends Base
             $table->string('desc', 128)
                 ->comment('菜单描述');
             $table->string('icon', 128)
-                ->->nullable()
+                ->nullable()
                 ->comment('菜单样式');
             $table->string('url', 128)
-                ->->nullable()
+                ->nullable()
                 ->comment('菜单路由');
             $table->softDeletes();
             $table->timestamps();
