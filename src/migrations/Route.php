546959877cebb6755899e7059d549542f4ff4eed
zhengyiping <987859263@qq.com>
2017-05-27 13:55:26 +0800

    create data table

---
+++ src/migrations/Route.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Migrations;
+
+/**
+* Menu Table Migration
+*/
+class Route extends Base
+{    
+    public function up()
+    {
+        $this->schema->create($this->table_name, function(\Illuminate\Database\Schema\Blueprint $table) {
+            $table->increments('id')
+                ->comment('主键ID');
+            $table->string('uid', 11)
+                ->comment('用户id');
+            $table->string('name', 128)
+                ->comment('菜单名称');
+            $table->string('desc', 128)
+                ->comment('菜单描述');
+            $table->string('icon', 128)
+                ->nullable()
+                ->comment('菜单样式');
+            $table->string('url', 128)
+                ->nullable()
+                ->comment('菜单路由');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+}
